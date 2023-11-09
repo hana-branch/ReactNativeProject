12c6dfac59e2df5291d83a540809b0423194915c
hana-branch <hpark@branch.io>
2023-11-09 15:31:13 -0800

    first

---
+++ App.tsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+// import type {PropsWithChildren} from 'react';
+import {
+  Button,
+  // SafeAreaView,
+  // ScrollView,
+  // StatusBar,
+  // StyleSheet,
+  Linking,
+  Text,
+  // useColorScheme,
+  View,
+} from 'react-native';
+// import {
+//   Colors,
+//   DebugInstructions,
+//   Header,
+//   LearnMoreLinks,
+//   ReloadInstructions,
+// } from 'react-native/Libraries/NewAppScreen';
+import branch from 'react-native-branch';
+
+function HomeScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Details"
+        onPress={() => navigation.navigate('Details')}
+      />
+    </View>
+  );
+}
+
+function DetailsScreen({navigation}) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  const linkData = {};
+  const linking = {
+    prefixes: ['/'],
+
+    // Custom function to get the URL which was used to open the app
+    async getInitialURL() {
+      const url = await Linking.getInitialURL();
+
+      return url;
+    },
+
+    // Custom function to subscribe to incoming links
+    subscribe(listener) {
+      // Listen to incoming links from Firebase Dynamic Links
+      const unsubscribeBranch = branch.subscribe({
+        onOpenStart: ({uri, cachedInitialEvent}) => {
+          console.log(
+            '[branch.io] subscribe onOpenStart, will open ' +
+              uri +
+              ' cachedInitialEvent is ' +
+              cachedInitialEvent,
+          );
+        },
+        onOpenComplete: ({error, params, uri}) => {
+          if (error) {
+            console.error(
+              '[branch.io] subscribe onOpenComplete, Error from opening uri: ' +
+                uri +
+                ' error: ' +
+                error,
+            );
+            return;
+          } else if (params) {
+            if (!params['+clicked_branch_link']) {
+              if (params['+non_branch_link']) {
+                console.log('[branch.io] non_branch_link: ' + uri);
+                return;
+              }
+            } else {
+              console.log(
+                `[branch.io] effect subscribe: ${JSON.stringify(params)}`,
+              );
+              return;
+            }
+          }
+        },
+      });
+
+      // Listen to incoming links from deep linking
+      const linkingSubscription = Linking.addEventListener('url', ({url}) => {
+        listener(url);
+      });
+
+      return () => {
+        // Clean up the event listeners
+        unsubscribeBranch();
+        linkingSubscription.remove();
+      };
+    },
+
+    config: {
+      // Deep link configuration
+    },
+  };
+
+  return (
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ README.md
@@ -0,0 +1,29 @@
+# Getting Started
+
+>**Note**: Make sure you have completed the [React Native - Environment Setup](https://reactnative.dev/docs/environment-setup) instructions till "Creating a new application" step, before proceeding.
+
+## Step 1: Install npm modules
+```bash
+npm install
+```
+
+## Step 2: Install Pods
+```bash
+cd ios
+pod install
+```
+
+## Step 3: Start the Metro Server
+```bash
+npm start
+```
+
+## Step 4: Start Application
+### For Android
+```bash
+npm run android
+```
+### For iOS
+```bash
+npm run ios
+```
